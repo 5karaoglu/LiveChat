6fdcbc797a6630e7e623777c80c3296016ae692c
Beşir Karaoğlu <50145106+5karaoglu@users.noreply.github.com>
2022-10-20 11:22:29 +0300

    add timber

--- app/src/main/java/com/besirkaraoglu/livechat/core/LCApplication.kt
+++ app/src/main/java/com/besirkaraoglu/livechat/core/LCApplication.kt
@@ -1,11 +1,16 @@
 package com.besirkaraoglu.livechat.core
 
 import android.app.Application
+import androidx.viewbinding.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class LCApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
